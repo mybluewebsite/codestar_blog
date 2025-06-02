fe31955067ba3ba4e0e74db316a231c8714cb3a8
mybluewebsite <mybluewebsite@dontsp.am>
2025-06-02 08:25:08 +0100

    heroku app setup

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8%m#*=&_f3$1g^zt-s=rodnp97kt$0!afever5*v34l@7_n@(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
