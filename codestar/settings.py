baac058fcaca64e293e0e825858ec68b7f215bcf
mybluewebsite <mybluewebsite@dontsp.am>
2025-06-02 08:27:59 +0100

    added the blog to installed apps

--- codestar/settings.py
+++ codestar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',  # Custom app for blog functionality
 ]
 
 MIDDLEWARE = [
