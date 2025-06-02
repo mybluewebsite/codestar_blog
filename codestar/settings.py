4aad250beb35c0edc62a4e60c15b0ae4be33a767
mybluewebsite <mybluewebsite@dontsp.am>
2025-06-02 08:33:34 +0100

    add to the requirements file

--- codestar/settings.py
+++ codestar/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # Custom app for blog functionality
+    'blog', # Custom app for blog functionality
 ]
 
 MIDDLEWARE = [
