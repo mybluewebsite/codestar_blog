40c42b758651787042180422d0e362ef06641a38
mybluewebsite <mybluewebsite@dontsp.am>
2025-06-13 20:48:21 +0100

    slight amednments made

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Post)
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+# Register your models here.
 admin.site.register(Comment)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+# Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
