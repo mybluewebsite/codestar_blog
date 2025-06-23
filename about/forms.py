3b9eb611ac7090a4c03c1fb548bbde9cffeab29b
mybluewebsite <mybluewebsite@dontsp.am>
2025-06-23 18:30:01 +0100

    added comment CRUD

--- about/forms.py
+++ about/forms.py
@@ -1,7 +1,8 @@
 from .models import CollaborateRequest
 from django import forms
 
-class CollaborateRequestForm(forms.ModelForm):
+
+class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
         fields = ('name', 'email', 'message')
--- about/views.py
+++ about/views.py
@@ -10,7 +10,11 @@ def about_me(request):
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+            messages.add_message(
+                request, 
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
